1b5ccd6ca144b99d9c4f484299eaf21413af544c
Abhay Giri <762006krishna@gmail.com>
2026-08-13 20:26:00 -0700

    Debugging Codes

---
+++ Day-13/main.py
@@ -0,0 +1,68 @@
+# debugging the code below
+"""def odd_or_even(number):
+
+    if number % 2 = 0:
+        return "This is an even number."
+    else:
+        return "This is an odd number."
+        """
+
+#correct code 
+def odd_or_even(number):
+    if number % 2 == 0:
+        return "This is an even number."
+    else:
+        return "This is an odd number."
+print(odd_or_even(10))
+
+#debugging the code below
+"""def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 4000 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+"""
+#correct code
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+
+#debugging the code below
+    """ # Target is the number up to which we count
+def fizz_buzz(target):
+    for number in range(1, target + 1):
+        if number % 3 == 0 or number % 5 == 0:
+            print("FizzBuzz")
+        if number % 3 == 0:
+            print("Fizz")
+        if number % 5 == 0:
+            print("Buzz")
+        else:
+            print([number])
+"""
+#correct code
+def fizz_buzz(target):
+    for number in range(1, target + 1):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
